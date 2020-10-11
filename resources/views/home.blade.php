37fa64cb736bb2a0e7c40e092b131e56b5ae167e
Calvin Then <samkurniawan1st@gmail.com>
2020-10-11 18:47:02 +0700

    Upload-an pertama
    11 oktober 2020

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    @if(!Auth::guest())
+                        Welcome to home dashboard
+                    @else
+                        You are not login or registered yet
+                    @endif
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
